5f244b1a429c303cf992f5af730c09fccf4facc5
yuxiong.zhang <yuxiong.zhang@corp.to8to.com>
2022-04-18 10:27:09 +0800

    feat: 集成husky校验

--- src/components/UsePinia.vue
+++ src/components/UsePinia.vue
@@ -2,7 +2,7 @@
  * @Author: yuxiong.zhang
  * @Date: 2022-04-14 17:26:02
  * @LastEditors: yuxiong.zhang
- * @LastEditTime: 2022-04-15 11:40:23
+ * @LastEditTime: 2022-04-15 15:38:45
 -->
 
 <template>
@@ -14,5 +14,5 @@
 import { useUserStore } from '@/store/user'
 
 const userStore = useUserStore()
-userStore.updateName('张三 -> 李四1')
+userStore.updateName('张三 -> 李四')
 </script>
